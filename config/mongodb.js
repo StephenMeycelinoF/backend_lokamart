ed9f0915f9e510030fb8bb1fe42fbb640032982d
Stephen Meycelino Fransisco <fstephenmeycelino@gmail.com>
2024-12-11 01:25:06 +0700

    fix: mongodb timeout

--- config/mongodb.js
+++ config/mongodb.js
@@ -4,7 +4,11 @@ const connectDB = async () => {
   mongoose.connection.on("connected", () =>
     console.log("MongoDB is connected")
   );
-  await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`);
+  await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+  });
 };
 
 export default connectDB;
